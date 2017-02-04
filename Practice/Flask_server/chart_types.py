86bd300e95f92cb36419b1cff1f195aa3131b53a
Mark <mrefermat@hotmail.com>
2017-02-04 18:43:36 +0000

    Extending the charts however some might not work

--- Practice/Flask_server/chart_types.py
+++ Practice/Flask_server/chart_types.py
@@ -22,3 +22,18 @@ def equity_markets_charts(div):
     data=df.ffill()['2016-6-30':]/df.ffill()['2016-6-30':].ix[0]
     return serialize(data,render_to=div,title='Equities YTD',output_type='json')
 
+def zscore_ranked(div):
+    store = Arctic('localhost')
+    store.initialize_library('FUTURES')
+    library = store['FUTURES']
+    data=pd.DataFrame()
+    for mkt in library.list_symbols():
+        try:
+            data[mkt]=library.read(mkt).data.Price
+        except:
+            print mkt
+    zscores=(data-pd.ewma(data,20))/pd.ewmstd(data,20)
+    latest=zscores.tail(2)
+    zscore_ranked=latest.T.sort_values(by=latest.T.columns[0]).dropna()[:2]
+    zscore_ranked=zscore_ranked.append(latest.T.sort_values(by=latest.T.columns[0]).dropna()[-2:])
+    return serialize(zscore_ranked, kind="bar",render_to=div, title="Noteable market moves")
