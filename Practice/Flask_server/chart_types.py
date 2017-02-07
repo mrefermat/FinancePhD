fcc497ea69ff55d5ffbbbf7fb623abcf1ed8d530
Mark <mrefermat@hotmail.com>
2017-02-07 21:27:53 +0000

    Corrected bug

--- Practice/Flask_server/chart_types.py
+++ Practice/Flask_server/chart_types.py
@@ -19,7 +19,7 @@ def equity_markets_charts(div):
             df[mkt]=library.read(mkt).data.Price
         except:
             print mkt        
-    data=df.ffill()['2016-6-30':]/df.ffill()['2016-6-30':].ix[0]
+    data=df.ffill()['2016':]/df.ffill()['2016':].ix[0]
     return serialize(data,render_to=div,title='Equities YTD',output_type='json')
 
 def zscore_ranked(div):
@@ -34,6 +34,6 @@ def zscore_ranked(div):
             print mkt
     zscores=(data-pd.ewma(data,20))/pd.ewmstd(data,20)
     latest=zscores.tail(2)
-    zscore_ranked=latest.T.sort_values(by=latest.T.columns[0]).dropna()[:2]
-    zscore_ranked=zscore_ranked.append(latest.T.sort_values(by=latest.T.columns[0]).dropna()[-2:])
-    return serialize(zscore_ranked, kind="bar",render_to=div, title="Noteable market moves")
+    zscore_ranked=latest.T.sort_values(by=latest.T.columns[0]).dropna()[:5]
+    zscore_ranked=zscore_ranked.append(latest.T.sort_values(by=latest.T.columns[0]).dropna()[-5:])
+    return serialize(zscore_ranked,render_to=div, kind="bar", title="Noteable market moves",output_type='json')
