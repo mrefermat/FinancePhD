be0eabadc82646739096a087c33bd511af8c3541
Mark <mrefermat@hotmail.com>
2017-02-07 20:41:09 +0000

    updating

--- Practice/Flask_server/app.py
+++ Practice/Flask_server/app.py
@@ -6,7 +6,7 @@ from pandas_highcharts.core import serialize
 from arctic import Arctic
 from pymongo import MongoClient
 from flask_bootstrap import Bootstrap
-from chart_types import equity_markets_charts
+from chart_types import *
 
 app = Flask(__name__)
 
@@ -31,25 +31,16 @@ def graph_data(chartID = 'chart_ID', chart_type = 'bar', chart_height = 500):
 	library = store['FUTURES']
 	df=library.read('Cotton').data[['Price','Volume']]
 	chart_data=serialize(df,render_to='chart1',title='Cotton',output_type='json',secondary_y = ["Volume"])
-	df=library.read('Butter').data[['Price','Volume']]
 	data2=serialize(df,render_to='chart2',title='Butter',output_type='json',secondary_y = ["Volume"])
 	return render_template('graph.html', chart1=chart_data, chart2=data2)
 
 @app.route('/test')
 def test(chartID = 'chart_ID', chart_type = 'bar', chart_height = 500):
-	# Using Arctic database to store
-	store = Arctic('localhost')
-	# Create the library - defaults to VersionStore
-	store.initialize_library('FUTURES')
-	# Access the library
-	library = store['FUTURES']
-	df=library.read('Cotton').data[['Price','Volume']]
-	chart_data=serialize(df,render_to='chart1',title='Cotton',output_type='json',secondary_y = ["Volume"])
-	df=library.read('Butter').data[['Price','Volume']]
+	chart_data=zscore_ranked('chart1')
 	data2=equity_markets_charts('chart2')
 	return render_template('graph.html', chart1=chart_data, chart2=data2)
 
 
 
 if __name__ == "__main__":
-    app.run(debug = True, host='0.0.0.0', port=5000, passthrough_errors=True)
+    app.run(debug = True, host='0.0.0.0', port=5000, passthrough_errors=True,threaded=True)
