85b6617696689b3702380165406e5ffed96c2851
Mark <mrefermat@hotmail.com>
2017-02-26 19:30:38 +0000

    checkpoint
    new Highstock

--- Practice/Flask_server/static/js/main.js
+++ Practice/Flask_server/static/js/main.js
@@ -1,68 +1,27 @@
-$(function() {
+$(document).ready(function() {    // Create the chart
+    Highcharts.stockChart('container', {
+        rangeSelector: {
+            selected: 1
+        },
+        series: series,
+        title:title,
+        credits:{text:'Brought to you by Mark'},
+        tooltip: {valueDecimals:2},
+        plotOptions:{series:{compare:'percent'}
+        }
+    })
+});
 
-    // If you need to specify any global settings such as colors or other settings you can do that here
-
-    // Build Chart A
-    $('#chart-A').highcharts({
-        chart: {
-            type: 'column'
+$(document).ready(function() {    // Create the chart
+    Highcharts.stockChart('container2', {
+        rangeSelector: {
+            selected: 1
         },
-        title: {
-            text: 'Chart A'
-        },
-        xAxis: {
-            categories: ['Jane', 'John', 'Joe', 'Jack', 'jim']
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Apple Consumption'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        credits: {
-            enabled: false
-        },
-        tooltip: {
-            shared: true
-        },
-        series: [{
-            name: 'Apples',
-            data: [5, 3, 8, 2, 4]            
-        }]
-    });
-
-    // Build Chart B
-    $('#chart-B').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Chart B'
-        },
-        xAxis: {
-            categories: ['Jane', 'John', 'Joe', 'Jack', 'jim']
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Miles during Run'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        credits: {
-            enabled: false
-        },
-        tooltip: {
-            shared: true
-        },
-        series: [{
-            name: 'Miles',
-            data: [2.4, 3.8, 6.1, 5.3, 4.1]
-        }]
-    });
+        series: series1,
+        title:title1,
+        credits:{text:'Brought to you by Mark'},
+        tooltip: {valueDecimals:2}
+        // plotOptions:{series:{compare:'percent'}
+        
+    })
 });
